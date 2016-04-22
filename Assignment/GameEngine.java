7e5c8a54c716e9bd36907d2631aed97d918c0e32
triphop35 <triphop7768@gmail.com>
2016-04-22 15:37:31 +0700

    #03 Create GameEngine&GamePanel

---
+++ Assignment/GameEngine.java
@@ -0,0 +1,64 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.Timer;
+
+
+public class GameEngine implements KeyListener{
+	GamePanel gp;
+		
+	private SpaceShip v;	
+	
+	private Timer timer;
+	
+	private long score = 0;
+	private double difficulty = 0.1;
+	
+	public GameEngine(GamePanel gp, SpaceShip v) {
+		this.gp = gp;
+		this.v = v;		
+		
+		gp.sprites.add(v);
+		
+		timer = new Timer(50, new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				process();
+			}
+		});
+		timer.setRepeats(true);
+		
+	}
+	
+	public void start(){
+		timer.start();
+	}
+
+	private void process(){
+		
+		Rectangle2D.Double vr = v.getRectangle();
+		Rectangle2D.Double er;
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		//do nothing
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		//do nothing		
+	}
+}
